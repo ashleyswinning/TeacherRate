506a4373bac8bd492f4c7bde7b75ee1584e14733
att2976 <att2976@att.net>
2019-04-09 22:31:59 -0400

    Started implimenting the contract functions. TODO: comment finish a few functions test functions

--- backend/contracts/TeacherRate.sol
+++ backend/contracts/TeacherRate.sol
@@ -4,23 +4,66 @@ contract TeacherRate {
 
 	mapping (bytes32 => uint8) public teacherRatings;
 
-	bytes32[] teachers;
+	// bytes32[] teachers;
 
-	// include something about student database for ratings page to verify they are students against "student database"
+	struct teacher {
+		uint8 id;
+		bytes32 name;
+		uint8[] teacherRatings;
+		string[] teacherReviews;
+	}
+
+	teacher[] teachers;
+
 
 	constructor(bytes32[] memory teacherEntries) public {
 		teachers = teacherEntries;
 	}
 
-	function getVotes(bytes32 teacher) view public returns (uint8) {
-		return teacherRatings[teacher]; 
+	function _createTeacher (bytes32 _name) public {
+		uint8 id = _generateID();
+		teachers.push(teacher(id, _name, null, null));
 	}
 
-	function placeVote(bytes32 teacher) public {
-		teacherRatings[teacher] += 1;
+	function _generateID () returns(uint8) internal {
+		if(teachers.length < 1){
+			return 0;
+		}
+		else return teachers.length;
+	}
+
+	function _submitReview (uint8 _teacherID, uint8 _rating, string _review) public {
+		if (_rating < 1 || _rating > 5){ //rating must be between 1 and 5
+			throw;
+		}
+		else {
+			teachers[_teacherID].teacherRatings.push(_rating);
+			teachers[_teacherID].teacherReviews.push(_review);
+		}
 	}
 	
-	function getTeachers() view public returns (bytes32[] memory){
+			
+
+	function _getTeacherRatings(uint8 _teacherID) view public returns (uint8) {
+		uint8 avg = 0;
+		uint8 len = teachers[_teacherID].teacherRatings.length;
+		for(uint i =0; i<len; i++){
+			avg += teachers[_teacherID].teacherRatings[i];
+		}
+		avg = avg / len;
+		return avg; 
+	}
+
+	function _getTeacherReviews (uint8 _id) returns(string[]) public {
+		return teachers[_id].teacherReviews;
+	}
+	
+
+	// function _placeVote(bytes32 _teacher) public {
+	// 	teacherRatings[_teacher] += 1;
+	// }
+	
+	function _getTeachers() view public returns (bytes32[] memory){
 		return teachers;
 	}
 }
