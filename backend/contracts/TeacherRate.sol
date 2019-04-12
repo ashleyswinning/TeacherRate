041cebaa25e120eabf1ae1c99faa0279b4476c0e
att2976 <att2976@att.net>
2019-04-12 19:44:12 -0400

    fixed a few compiler errors, cant return a dynamic array of strings

--- backend/contracts/TeacherRate.sol
+++ backend/contracts/TeacherRate.sol
@@ -2,12 +2,12 @@ pragma solidity ^0.5.0;
 
 contract TeacherRate {
 
-	mapping (bytes32 => uint8) public teacherRatings;
+	// mapping (bytes32 => uint8) public teacherRatings;
 
 	// bytes32[] teachers;
 
 	struct teacher {
-		uint8 id;
+		uint256 id;
 		bytes32 name;
 		uint8[] teacherRatings;
 		string[] teacherReviews;
@@ -17,24 +17,29 @@ contract TeacherRate {
 
 
 	constructor(bytes32[] memory teacherEntries) public {
-		teachers = teacherEntries;
+		// teachers = teacherEntries;
 	}
 
 	function _createTeacher (bytes32 _name) public {
-		uint8 id = _generateID();
-		teachers.push(teacher(id, _name, null, null) -1);
+		uint256 _id = _generateID();
+		teachers.push(teacher({
+			id: _id,
+			name: _name,
+			teacherRatings: new uint8[](0),
+			teacherReviews: new string[](0)
+			}) -1);
 	}
 
-	function _generateID () returns(uint8) internal {
+	function _generateID () internal returns(uint256) {
 		if(teachers.length < 1){
 			return 0;
 		}
 		else return teachers.length;
 	}
 
-	function _submitReview (uint8 _teacherID, uint8 _rating, string _review) public {
+	function _submitReview (uint8 _teacherID, uint8 _rating, string memory _review) view public {
 		if (_rating < 1 || _rating > 5){ //rating must be between 1 and 5
-			throw;
+			revert();
 		}
 		else {
 			teachers[_teacherID].teacherRatings.push(_rating) -1;
@@ -44,9 +49,9 @@ contract TeacherRate {
 	
 			
 
-	function _getTeacherRatings(uint8 _teacherID) view public returns (uint8) {
-		uint8 avg = 0;
-		uint8 len = teachers[_teacherID].teacherRatings.length;
+	function _getTeacherRatings(uint8 _teacherID) view public returns (uint256) {
+		uint256 avg = 0;
+		uint256 len = teachers[_teacherID].teacherRatings.length;
 		for(uint i =0; i<len; i++){
 			avg += teachers[_teacherID].teacherRatings[i];
 		}
@@ -54,7 +59,7 @@ contract TeacherRate {
 		return avg; 
 	}
 
-	function _getTeacherReviews (uint8 _id) returns(string[]) public {
+	function _getTeacherReviews (uint8 _id) view public returns(string[] memory) {
 		return teachers[_id].teacherReviews;
 	}
 	
@@ -63,7 +68,7 @@ contract TeacherRate {
 	// 	teacherRatings[_teacher] += 1;
 	// }
 	
-	function _getTeachers() view public returns (bytes32[] memory){
+	function _getTeachers() view public returns (teacher[] memory){
 		return teachers;
 	}
 }
