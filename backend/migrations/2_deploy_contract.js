0ee65d4f353b6999119b2dff8da59bb1a5d4a42f
ashleyswinning <ash.huynh@uga.edu>
2019-04-15 21:40:57 -0400

    added in updated components to make it work

--- backend/migrations/2_deploy_contract.js
+++ backend/migrations/2_deploy_contract.js
@@ -1,5 +1,13 @@
 var TeacherRate = artifacts.require("./TeacherRate.sol");
 
 module.exports = function(deployer) {
-	deployer.deploy(TeacherRate);
+	deployer.deploy(TeacherRate)
+	.then(() => TeacherRate.deployed())
+	.then(function(contract) {
+		contract._createTeacher("Ron", "Swanson");
+		contract._createTeacher("Robin", "Williams");
+		contract._createTeacher("Reese", "Witherspoon");
+		contract._createTeacher("Dwight", "Schrute");
+		contract._createTeacher("Betty", "White");
+	});
 };
